031a90aa1c57404d9b0bbcbc4d3c85a683742f04
ROCKTAKEY <rocktakey@gmail.com>
2022-09-12 22:08:12 +0900

    Initial commit

---
+++ mic.el
@@ -0,0 +1,38 @@
+;;; mic.el --- Minimal configuration manager  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords: convenience
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; URL: https://github.com/ROCKTAKEY/mic
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Minimal configuration manager
+
+;;; Code:
+
+(defgroup mic ()
+  "Minimal configuration manager"
+  :group 'convenience
+  :prefix "mic-"
+  :link '(url-link "https://github.com/ROCKTAKEY/mic"))
+
+(provide 'mic)
+;;; mic.el ends here
---
+++ test/mic-test.el
@@ -0,0 +1,40 @@
+;;; mic-test.el --- Test for mic
+
+;; Copyright (C) 2022  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Test for mic
+
+;;; Code:
+
+(require 'ert)
+
+(require 'undercover)
+(undercover "*.el"
+            (:report-format 'codecov)
+            (:report-file "coverage-final.json")
+            (:send-report nil))
+
+(require 'mic)
+
+
+
+
+(provide 'mic-test)
+;;; mic-test.el ends here
