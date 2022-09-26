bc28de4c91b4920d2f2e088b26a2834eca18d9bb
ROCKTAKEY <rocktakey@gmail.com>
2022-09-27 01:39:01 +0900

    docs: Update version

--- mic.el
+++ mic.el
@@ -5,7 +5,7 @@
 ;; Author: ROCKTAKEY <rocktakey@gmail.com>
 ;; Keywords: convenience
 
-;; Version: 0.13.4
+;; Version: 0.14.0
 ;; Package-Requires: ((emacs "26.1"))
 ;; URL: https://github.com/ROCKTAKEY/mic
 
