5931ce4d5e93290ec0bcb7e6980cc5726ab0b259
ROCKTAKEY <rocktakey@gmail.com>
2022-10-28 02:04:47 +0900

    test: Remove annoying test

--- test/mic-deffilter-test.el
+++ test/mic-deffilter-test.el
@@ -27,6 +27,7 @@
 ;;; Code:
 
 (require 'ert)
+(require 'cl-lib)
 
 (require 'undercover)
 (undercover "*.el"
@@ -50,29 +51,6 @@ The test defined by this expands macro once."
 
 
 
-(mic-ert-macroexpand-1 mic-deffilter-const-macroexpand-1
-  ((mic-deffilter-const func-name
-     :foo t
-     :bar '(2 4))
-   . (defun func-name (plist)
-       "Filter for `mic'.
-It return PLIST but each value on some property below is replaced:
-(:foo t :bar
-      '(2 4))
-"
-       (mic-plist-put plist :foo t)
-       (mic-plist-put plist :bar '(2 4))
-       plist))
-  ((mic-deffilter-const func-name
-     "docstring"
-     :foo t
-     :bar '(2 4))
-   . (defun func-name (plist)
-       "docstring"
-       (mic-plist-put plist :foo t)
-       (mic-plist-put plist :bar '(2 4))
-       plist)))
-
 (ert-deftest mic-deffilter-const ()
   (mic-deffilter-const mic-test-mic-deffilter-const
     :foo t
@@ -82,33 +60,6 @@ It return PLIST but each value on some property below is replaced:
          (result (mic-test-mic-deffilter-const init)))
     (should (equal (plist-get result :foo) t))
     (should (equal (plist-get result :bar) '(2 4)))))
-
-(mic-ert-macroexpand-1 mic-deffilter-const-append-macroexpand-1
-  ((mic-deffilter-const-append func-name
-     :foo '(t)
-     :bar '(2 4))
-   . (defun func-name (plist)
-       "Filter for `mic'.
-It return PLIST but each value on some property below is appended:
-(:foo
- '(t)
- :bar
- '(2 4))
-"
-       (mic-plist-put-append plist :foo
-                              '(t))
-       (mic-plist-put-append plist :bar
-                              '(2 4))
-       plist))
-  ((mic-deffilter-const-append func-name
-     "docstring"
-     :foo '(t)
-     :bar '(2 4))
-   . (defun func-name (plist)
-       "docstring"
-       (mic-plist-put-append plist :foo '(t))
-       (mic-plist-put-append plist :bar '(2 4))
-       plist)))
 
 (ert-deftest mic-deffilter-const-append ()
   (mic-deffilter-const-append mic-test-mic-deffilter-const-append
@@ -120,31 +71,6 @@ It return PLIST but each value on some property below is appended:
     (should (equal (plist-get result :foo) '(1 t)))
     (should (equal (plist-get result :bar) '(2 3 4)))))
 
-(mic-ert-macroexpand-1 mic-deffilter-t-to-name-macroexpand-1
-  ((mic-deffilter-t-to-name func-name :foo)
-   . (defun func-name (plist)
-       "Filter for `mic'.
-It return PLIST but element t of the list value on :foo
-is replaced to the value on `:name'."
-       (mic-plist-put plist :foo
-                      (mapcar
-                       (lambda (arg)
-                         (if (eq arg t)
-                             (plist-get plist :name)
-                           arg))
-                       (plist-get plist :foo)))))
-  ((mic-deffilter-t-to-name func-name :foo
-     "docstring")
-   . (defun func-name (plist)
-       "docstring"
-       (mic-plist-put plist :foo
-                      (mapcar
-                       (lambda (arg)
-                         (if (eq arg t)
-                             (plist-get plist :name)
-                           arg))
-                       (plist-get plist :foo))))))
-
 (ert-deftest mic-deffilter-t-to-name ()
   (mic-deffilter-t-to-name mic-test-mic-deffilter-t-to-name :foo)
 
@@ -153,54 +79,18 @@ is replaced to the value on `:name'."
     (should (equal (plist-get result :foo) '(1 name x)))
     (should (equal (plist-get result :bar) '(4)))))
 
+(ert-deftest mic-deffilter-validate ()
+  (mic-deffilter-validate mic-test-mic-deffilter-validate
+    :foo :bar)
 
-(mic-ert-macroexpand-1 mic-deffilter-validate
-  ((mic-deffilter-validate filter-name
-     "docstring"
-     :keyword1
-     :keyword2
-     :keyword3)
-   . (defun filter-name
-         (plist)
-       "docstring"
-       (let (result)
-         (while plist
-           (let ((key (pop plist))
-                 (value (pop plist)))
-             (if (not
-                  (memq key (append
-                             '(:keyword1 :keyword2 :keyword3)
-                             '(:name))))
-               (warn "`mic' %s: The keyword %s is not allowed by filter `%s'"
-                     (plist-get plist :name) key 'filter-name)
-               (push key result)
-               (push value result))))
-         (nreverse result))))
-  ((mic-deffilter-validate filter-name
-     :keyword1
-     :keyword2
-     :keyword3)
-   . (defun filter-name
-         (plist)
-       "Filter for `mic'.
-It validates PLIST properties and warn if PLIST has invalid properties.
-The valid properties are:
-`:keyword1'
-`:keyword2'
-`:keyword3'"
-       (let (result)
-         (while plist
-           (let ((key (pop plist))
-                 (value (pop plist)))
-             (if (not
-                  (memq key (append
-                             '(:keyword1 :keyword2 :keyword3)
-                             '(:name))))
-               (warn "`mic' %s: The keyword %s is not allowed by filter `%s'"
-                     (plist-get plist :name) key 'filter-name)
-               (push key result)
-               (push value result))))
-         (nreverse result)))))
+  (let* ((init '(:foo t :bar 2))
+         (result (mic-test-mic-deffilter-validate init)))
+    (should (equal (plist-get result :foo) t))
+    (should (equal (plist-get result :bar) 2)))
+
+  (let* ((init '(:foo 1 :hoge 2)))
+    (cl-letf (((symbol-function 'warn) (lambda (&rest arg) (error "Error"))))
+      (should-error (mic-test-mic-deffilter-validate init)))))
 
 (provide 'mic-deffilter-test)
 ;;; mic-deffilter-test.el ends here
